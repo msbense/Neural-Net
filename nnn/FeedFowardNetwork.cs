ce458b53572fee784446f46ccebc1594fb7f0661
Mark Bense <Mark Bense>
2016-01-01 20:48:34 -0500

    Created base class NeuralNetwork, moved several variables from FFN to this new class, created RecurrentNetwork class, changed both network classes to inherit from NeuralNetwork

--- nnn/FeedFowardNetwork.cs
+++ nnn/FeedFowardNetwork.cs
@@ -1,21 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace nnn
 {
     /// <summary>
     /// Feedfoward neural network
     /// </summary>
-    public class FeedFowardNetwork
+    public class FeedFowardNetwork : NeuralNetwork
     {
         public List<List<Neuron>> Neurons;
-        public double LearningConstant { get; set; }
-        public double RegularizationConstant { get; set; }
-        public double totalTrainingSize { get; set; }
-        public double miniBatchSize { get; set; }
+
         public FeedFowardNetwork(params int[] structure)
         {
             Random rng = new Random();
@@ -131,46 +125,6 @@ namespace nnn
                         n.InputWeights[weightIndex] = n.InputWeights[weightIndex] * (1 - ((LearningConstant * RegularizationConstant) / totalTrainingSize)) - LearningConstant * n.Error * Neurons[layerIndex - 1][weightIndex].Activation;
                     }
                 }
-            }
-
-        }
-
-        /// <summary>
-        /// Cost Prime, derivative of the cost Function
-        /// </summary>
-        public double cp(double value, double correct) { return (value - correct); }
-        /// <summary>
-        /// Cross Entropy Prime, derivative of the cross entropy cost function
-        /// </summary>
-        public double cep(double value, double correct) { return (value - correct); }
-        /// <summary>
-        /// Cross Entropy cost function
-        /// </summary>
-        public double ce(double value, double correct) { return (-correct * Math.Log(value)) - ((1 - correct) * Math.Log(1 - value)); }
-        /// <summary>
-        /// Cross Entropy cost function with regularization
-        /// </summary>
-        public double ceRegularized(double value, double correct) { return (-correct * Math.Log(value)) - ((1 - correct) * Math.Log(1 - value)) + ((RegularizationConstant / (2 * totalTrainingSize)) * Neurons.Sum((l) => { return l.Sum((n) => { return n.InputWeights.Sum(); }); })); }
-        /// <summary>
-        /// The Cost function 
-        /// </summary>
-        public double c(double value, double correct) { return .5 * Math.Pow(correct - value, 2); }
-        /// <summary>
-        /// Sigmoid function
-        /// </summary>
-        public double sigma(double value) { return (1 / (1 + Math.Exp(-value))); }
-        /// <summary>
-        /// The derivative of the Sigmoid function
-        /// </summary>
-        public double sigmaPrime(double value)
-        {
-            if (double.IsInfinity(Math.Pow((1 + Math.Exp(-value)), 2)))
-            {
-                return 0;
-            }
-            else
-            {
-                return (Math.Exp(-value) / (Math.Pow((1 + Math.Exp(-value)), 2)));
             }
 
         }
---
+++ nnn/NeuralNetwork.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nnn
+{
+    public abstract class NeuralNetwork
+    {
+        public double LearningConstant { get; set; }
+        public double RegularizationConstant { get; set; }
+        public double totalTrainingSize { get; set; }
+        public double miniBatchSize { get; set; }
+
+        /// <summary>
+        /// Cost Prime, derivative of the cost Function
+        /// </summary>
+        public double cp(double value, double correct) { return (value - correct); }
+        /// <summary>
+        /// Cross Entropy Prime, derivative of the cross entropy cost function
+        /// </summary>
+        public double cep(double value, double correct) { return (value - correct); }
+        /// <summary>
+        /// Cross Entropy cost function
+        /// </summary>
+        public double ce(double value, double correct) { return (-correct * Math.Log(value)) - ((1 - correct) * Math.Log(1 - value)); }
+        /// <summary>
+        /// Cross Entropy cost function with regularization
+        /// </summary>
+        public double ceRegularized(double value, double correct) { return (-correct * Math.Log(value)) - ((1 - correct) * Math.Log(1 - value)) + ((RegularizationConstant / (2 * totalTrainingSize)) * Neurons.Sum((l) => { return l.Sum((n) => { return n.InputWeights.Sum(); }); })); }
+        /// <summary>
+        /// The Cost function 
+        /// </summary>
+        public double lsc(double value, double correct) { return .5 * Math.Pow(correct - value, 2); }
+        /// <summary>
+        /// Sigmoid function
+        /// </summary>
+        public double sigma(double value) { return (1 / (1 + Math.Exp(-value))); }
+        /// <summary>
+        /// The derivative of the Sigmoid function
+        /// </summary>
+        public double sigmaPrime(double value)
+        {
+            if (double.IsInfinity(Math.Pow((1 + Math.Exp(-value)), 2)))
+            {
+                return 0;
+            }
+            else
+            {
+                return (Math.Exp(-value) / (Math.Pow((1 + Math.Exp(-value)), 2)));
+            }
+
+        }
+    }
+}
